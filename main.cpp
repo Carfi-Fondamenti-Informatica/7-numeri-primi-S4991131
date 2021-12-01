1b986246245a30d1f70b4eb5ca6b8a2e8e88c51d
S4991131 <91836509+S4991131@users.noreply.github.com>
2021-12-01 07:51:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@ using namespace std;
 int main(){
 
     int numero=0, conta=0;
-    cin >> numero;
+    cin>>numero;
      
     primo (numero, 1, conta);
     if (conta==0){
-        cout << "numero primo" << endl;
+        cout<<"numero primo"<<endl;
     } else {
-        cout << "numero non primo" << endl;
+        cout<<"numero non primo"<<endl;
     }
   return 0;
 }

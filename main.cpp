2f827670df8e1400dfc8f6473e209efbad844788
S4991131 <91836509+S4991131@users.noreply.github.com>
2021-12-01 17:05:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,17 +2,18 @@
 #include "lib.h"
 using namespace std;
 
-int main(){
-
+int main (){
+    
     int numero=0, conta=0;
     cin>>numero;
-     
+    
     primo (numero, 1, conta);
     if (conta==0){
         cout<<"numero primo"<<endl;
         
     } else {
         cout<<"numero non primo"<<endl;
-    }
-  return 0;
+    }    
+    return 0;
 }
+

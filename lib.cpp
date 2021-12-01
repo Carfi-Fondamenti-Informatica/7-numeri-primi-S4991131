f8604a7507a105c1caded1ec076a61a99bd44ec8
S4991131 <91836509+S4991131@users.noreply.github.com>
2021-12-01 17:24:48 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,2 +1,14 @@
-
-void primo (int a, int i, int &conta);
+#include "lib.h"
+void primo (int a, int i, int &conta){
+  int div=0;
+  i++;
+  if (a>1 and a>i){
+    div=a%i;
+    if (div==0){
+      conta++;
+      
+  } else {
+      primo (a, i, conta);
+  }
+ }
+}  
